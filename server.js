8bc9e2fd41df698451eed3b03a66348bdfc80f4a
wym524 <wym524@users.noreply.github.com>
2026-06-08 18:26:26 +0000

    feat: 链接服务器
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- server.js
+++ server.js
@@ -251,13 +251,6 @@ const initialWriting = [
 // ---------- Express 中间件 ----------
 app.use(express.json());
 app.use(cookieParser());
-app.use((req, res, next) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Cookie');
-  next();
-});
 app.use(express.static(path.join(__dirname, 'public')));
 
 // ---------- Session 解析工具 ----------
